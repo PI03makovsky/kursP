50fecd381e67cc11e7b3fb5331291b5e7f7467ab
PI03makovsky <vow.makowsky2014@gmail.com>
2022-01-17 23:57:06 +0700

    r

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class program
+    {
+        static void Main(string[] args)
+        { //date rez=new date();
+        //int a;
+        //a = rez.date();
+        Console.WriteLine("hello");
+        }
+    }
+}
